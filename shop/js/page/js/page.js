a94364dc8963ecde64d8f0b1f0d15db31e6120a5
Lucas1860 <153546777+Lucas1860@users.noreply.github.com>
2023-12-12 03:12:58 +0300

    Update page.js

--- shop/js/page/js/page.js
+++ shop/js/page/js/page.js
@@ -1,5 +1,5 @@
 function rev(num, num2){
     const one = document.querySelector('.one');
-    one.outerHTML = `<img class="one" src="C:/Users/avaki/OneDrive/Рабочий стол/ювелирка/Украшения/${num}.${num2}.jpg">`;
+    one.outerHTML = `<img class="one" src="img/${num}.${num2}.jpg">`;
 }
 
