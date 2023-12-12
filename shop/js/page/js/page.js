6af9512493e7244414cbd4403cd7266878c36e43
Lucas1860 <153546777+Lucas1860@users.noreply.github.com>
2023-12-12 03:03:28 +0300

    Add files via upload

---
+++ shop/js/page/js/page.js
@@ -0,0 +1,5 @@
+function rev(num, num2){
+    const one = document.querySelector('.one');
+    one.outerHTML = `<img class="one" src="C:/Users/avaki/OneDrive/Рабочий стол/ювелирка/Украшения/${num}.${num2}.jpg">`;
+}
+
