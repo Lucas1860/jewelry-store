51a65a146d52772d8b4f818a07f7946322d311d9
Lucas1860 <153546777+Lucas1860@users.noreply.github.com>
2023-12-14 03:48:37 +0300

    Update shop.js

--- shop/js/shop.js
+++ shop/js/shop.js
@@ -124,3 +124,17 @@ function changeQuantity(key, quantity){
     reloadCard();
 }
 
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+
+
+
